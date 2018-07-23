4fe9f8b4268e85fa76dc31f17d2faeb4784db572
aks97cs <aks97cs@gmail.com>
2018-07-24 02:22:44 +0530

    Issue resolve

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -148,11 +148,19 @@
      
     move_uploaded_file($product_image_tmp, "product_images/$product_image");   
        
-    echo  $insert_product="insert into products(product_id,product_cat,product_brand,product_title,product_price,product_desc,product_image,product_keywords) values(1,'$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image',' $product_keywords')";
+   $product_id = 1;
+   $query = "insert into products values ('$product_id','$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
+ /*  echo "products title = ".$product_title;
+   echo "act = ".$product_cat;
+   echo "brand = ".$product_brand;
+   echo "price = ".$product_price;
+   echo "desc = ".$product_desc;
+   echo "keywords = ".$product_keywords;
+   echo "image name ".$product_image;
+*/
 
 
-
-  mysqli_query($con,$insert_product) or die ("can not work!"); 
+  mysqli_query($con,$query) or die ("can not work!"); 
 
   echo "<script>alert('Product has been inserted')</script>";
   echo "<script> window.open('insert_product.php','self')</script>";
