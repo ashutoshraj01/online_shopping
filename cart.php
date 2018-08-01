11ce343de0ac8c7510bcea91ac0ab37cfa04a56e
logan01 <ashutoshraj908@gmail.com>
2018-08-02 01:48:53 +0530

    adding remove functionality in the cart section

--- cart.php
+++ cart.php
@@ -92,7 +92,7 @@ include("functions/functions.php");
 			              <div >
 			                   <br>
                               <form action="" method="post" enctype="multipart/form-data">  
-                                   <table 	align="center" width="700" >
+                                   <table  align="center" width="700" >
         							                             
                                         <tr style="color: blue">
 
@@ -132,7 +132,7 @@ include("functions/functions.php");
 																	            $total +=$values;
                                                ?>
                                              <tr align="center">
-                                             	   <td><input type="checkbox" name="remove[]"/></td>	
+                                             	   <td><input type="checkbox" name="remove[]" value="<?php echo "$pro_id"; ?>" /></td>	
                                              	   <td><?php echo $product_title;  ?><br>
                                                      <img src="admin/product_images/<?php echo $product_image; ?>" width="80" height="80" />
                                              	   </td>
@@ -152,11 +152,55 @@ include("functions/functions.php");
                                               	<td align="right"><?php echo $total; ?></td>
                                               </tr>  
 
+                                               <tr align="center">
+                                               	    <td colspan="2"><input type="submit" name="update_cart" value="Update Cart"> </td>
+                                               	    <td ><input type="submit" name="continue" value="Continue Shopping"></td>
+                                               	    <td ><button><a href="checkout.php" style="text-decoration: none; color: black" >Checkout</a></button></td>	
+                                               	 
+                                               </tr>
+
                                     </table>  
                                      
 
        
                              </form>
+
+                        <?php
+
+                         if(isset($_POST['update_cart']))
+                         {
+                            foreach ($_POST['remove'] as $remove_id) 
+                            {
+                               $delete_product="delete from cart where p_id='$remove_id' and ip_add='$ip'";
+                               $run_delete=mysqli_query($con,$delete_product);
+
+                               if($run_delete)
+                                 {
+                                    echo "<script>window.open('cart.php','_self')</script>";
+
+
+                                 }  
+                              
+                            
+                            }
+
+
+
+
+                         }
+
+                                  if(isset($_POST['continue']))
+                                  	 {
+
+                                         echo "<script>window.open('index.php','_self')</script>";
+
+                                  	 }
+  
+
+
+                        ?>
+
+
 			              </div>
           </div>
 
