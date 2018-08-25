1ed60dbacad4609498007776f2eb06b6a9c06503
logan01 <ashutoshraj908@gmail.com>
2018-08-25 23:07:12 +0530

    adding admin login form

---
+++ admin_area/login.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form</title>
+
+<link rel="stylesheet" type="text/css" href="styles/login_style.css" media="all">
+
+
+
+
+</head>
+<body>
+
+
+
+
+<div class="login">
+	<h1>Admin Login</h1>
+    <form method="post">
+    	<input type="text" name="u" placeholder="Username" required="required" />
+        <input type="password" name="p" placeholder="Password" required="required" />
+        <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+    </form>
+</div>
+
+
+
+</body>
+</html>
+
+  
